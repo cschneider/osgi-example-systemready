e5e8b003221b708dbb4182628764f1fea4704486
Christian Schneider <cschneid@adobe.com>
2018-08-08 18:08:42 +0200

    fix

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 # Replace cschneider with your docker login
 export TAG=cschneider/osgi.example.kubernetes
-
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 docker build -t $TAG starter
 docker push $TAG
