893c493762aad1715aa4b421b675eb9711812708
Christian Schneider <cschneid@adobe.com>
2018-08-15 11:08:01 +0200

    Use env var for docker username

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-export TAG=dockeradulvac.azurecr.io/osgi.example.kubernetes
+export TAG=$DOCKER_USERNAME/osgi.example.kubernetes
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 docker build -t $TAG starter
 docker push $TAG
